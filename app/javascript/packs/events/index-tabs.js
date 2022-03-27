aad8348994cdce758bcf358f2203711ebba8658c
Aleksandr Novikov <novikovav2@gmail.com>
2022-03-27 16:10:21 +1000

    Some CSS editions

--- app/javascript/packs/events/index-tabs.js
+++ app/javascript/packs/events/index-tabs.js
@@ -7,10 +7,14 @@ activeLink.addEventListener('click', (e) => {
     e.preventDefault()
     activeDiv.style.display = 'block'
     archivedDiv.style.display = 'none'
+    activeLink.classList.add('status-links_active')
+    archivedLink.classList.remove('status-links_active')
 })
 
 archivedLink.addEventListener('click', (e) => {
     e.preventDefault()
     activeDiv.style.display = 'none'
     archivedDiv.style.display = 'block'
+    activeLink.classList.remove('status-links_active')
+    archivedLink.classList.add('status-links_active')
 })
